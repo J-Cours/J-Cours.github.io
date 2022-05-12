7d875cc4acb154706ad4de1c5e7c3b15122db962
J-Cours <jeremy.cours.2@gmail.com>
2022-05-12 12:18:04 +0200

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,32 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Melolontha hippocastani studies in northern Vosges mountains",
+    48.908677,
+    7.421408
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "CLIMTREE project - forest dieback in spruce forests",
+    48.994157,
+    13.399582
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "CLIMTREE project - forest dieback in fir forests",
+    42.811653,
+    0.590468
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "CLIMTREE project - forest dieback in fir forests",
+    42.831889,
+    1.970911
+  ],
+  [
+    "CANOPEE project - forest dieback in oak forest",
+    47.268166,
+    2.088917
+  ],
+  [
+    "CANOPEE project - forest dieback in oak forest",
+    48.030079,
+    2.153492
   ]
 ];
