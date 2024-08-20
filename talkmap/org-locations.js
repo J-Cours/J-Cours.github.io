3690c0b7c27ea5b229726108383017e9ed26862c
J-Cours <jeremy.cours.2@gmail.com>
2024-08-20 10:28:22 +0300

    add photo page

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -5,37 +5,37 @@ var addressPoints = [
     7.421408
   ],
   [
-    "CLIMTREE project - forest dieback in spruce forests <a href='/publication/2021-02-15-paper'>[paper1]</a>",
+    "CLIMTREE project - forest dieback in spruce forests <a href='/publication/2021-02-15-paper'>[paper1]</a> <a href='/publication/2024-01-paper'>[paper2]</a>",
     48.994157,
     13.399582
   ],
   [
-    "CLIMTREE project - forest dieback in fir forests <a href='/publication/2021-02-15-paper'>[paper1]</a> <a href='/publication/2022-01-18-paper'>[paper2]</a> <a href='/publication/2022-05-01-paper'>[paper3]</a>",
+    "CLIMTREE project - forest dieback in fir forests <a href='/publication/2021-02-15-paper'>[paper1]</a> <a href='/publication/2022-01-18-paper'>[paper2]</a> <a href='/publication/2022-06-19-paper'>[paper3]</a> <a href='/publication/2024-01-paper'>[paper4]</a>",
     42.811653,
     0.590468
   ],
   [
-    "CLIMTREE project - forest dieback in fir forests <a href='/publication/2021-02-15-paper'>[paper1]</a> <a href='/publication/2022-01-18-paper'>[paper2]</a> <a href='/publication/2022-05-01-paper'>[paper3]</a>",
+    "CLIMTREE project - forest dieback in fir forests <a href='/publication/2021-02-15-paper'>[paper1]</a> <a href='/publication/2022-01-18-paper'>[paper2]</a> <a href='/publication/2022-06-19-paper'>[paper3]</a> <a href='/publication/2024-01-paper'>[paper4]</a>",
     42.831889,
     1.970911
   ],
   [
-    "CANOPEE project - forest dieback in oak forest",
+    "CANOPEE project - forest dieback in oak forest <a href='/publication/2024-01-paper'>[paper1]</a>",
     47.268166,
     2.088917
   ],
   [
-    "CANOPEE project - forest dieback in oak forest",
+    "CANOPEE project - forest dieback in oak forest <a href='/publication/2024-01-paper'>[paper1]</a>",
     48.030079,
     2.153492
   ],
   [
-    "Thesis - INRAE EFNO",
+    "PhD Thesis - INRAE EFNO",
     47.835041,
     2.760474
   ],
   [
-    "Postdoctoral fellowship - Jyvaskyla University",
+    "Postdoctoral Researcher - Jyvaskyla University",
     62.235808,
     25.731399
   ]
