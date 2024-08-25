122907b96c211e831acaca2966b7e4b0e9e504e1
J-Cours <jeremy.cours.2@gmail.com>
2024-08-25 14:09:30 +0300

    update_photo_gallery

---
+++ _sass/_photo.js
@@ -0,0 +1,12 @@
+/* ==========================================================================
+   JS doc for photo gallery
+   ========================================================================== */
+
+$(".images img").click(function(){
+  $("#full-image").attr("src", $(this).attr("src"));
+  $('#image-viewer').show();
+});
+
+$("#image-viewer .close").click(function(){
+  $('#image-viewer').hide();
+});
